19ed981b5372b0a6787dc584e079ceac4383c06b
s-moreau00 <moreau.sylvain6969@gmail.com>
2023-12-04 23:21:14 +0100

    quete 03 composant PokemonCard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,35 @@
-function PokemonCard() {
-    return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbizarre" />
-            <figcaption>Bulbizarre</figcaption>
-        </figure>
-    );
-}
+import { useState } from "react";
 
-export default PokemonCard;
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      src:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+    {
+        name: "salalamèche",
+        src:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+  ];
+
+  export default function PokemonCard() {
+    //statement
+    //copie de pokemonList dans un tableau, le rendre dynamique avec useState 
+    const [pokemons, setPokemons] = useState([...pokemonList])
+    //comportement
+
+    //render
+    return (
+        pokemons[2].src ? ( <>
+            <figure>
+                <img src={pokemons[2].src} alt={pokemons[2].name} />
+                 <figcaption>{pokemons[2].name}</figcaption>
+            </figure>
+            </>) : ( <>
+                <p>???</p>
+                <p>{pokemons[2].name}</p>
+                </>) ) 
+                }
