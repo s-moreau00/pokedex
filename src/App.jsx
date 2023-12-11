8aba5c7de15a612245296c09ebe3874e8aa8ab35
s-moreau00 <moreau.sylvain6969@gmail.com>
2023-12-11 23:53:07 +0100

    exercice state01 pokedex react

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,51 @@
 import './App.css'
 import PokemonCard from "./components/PokemonCard"
+import React, {useState} from 'react';
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    src:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-  {
-      name: "salalamèche",
-      src:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      name: "bulbasaur",
+      src:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
-];
+    {
+      name: "charmander",
+      src:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      src:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      src:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
-function App() {
+  export default function App() {
+  //state
+const [pokemonIndex, setPokemonIndex] = useState(0);
+  //comportements
+const precedent = (e) => {
+  if(pokemonIndex > 0)
+    setPokemonIndex(pokemonIndex - 1)
+}
+const suivant = (e) => {
+ if(pokemonIndex < pokemonList.length - 1)
+  setPokemonIndex(pokemonIndex + 1)
+}
+  //render
   return (
      <div>
-      <PokemonCard pokemon={pokemonList[2]}/>
+      { pokemonIndex > 0 ? (<button onClick={precedent}>précèdent</button>) : (<></>)}
+      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+      { pokemonIndex < pokemonList.length - 1 ? (<button onClick={suivant}>suivant</button>) : (<></>)}
      </div>
   )
 }
-
-export default App;
